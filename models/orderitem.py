90ca32c9a1cdfdf52849a0267bca06a725c557d3
bruno <brunosgazineu@hotmail.com>
2023-10-25 20:35:14 -0300

    feat: orderitem implementation

--- models/orderitem.py
+++ models/orderitem.py
@@ -1,2 +1,17 @@
+from models.order import Order
+from models.product import Product
+
+
 class OrderItem:
-    ...
+    def __init__(
+        self,
+        *,
+        quantity: int,
+        unitary_price: float,
+        order: Order,
+        product: Product
+    ):
+        self.quantity = quantity
+        self.unitary_price = unitary_price
+        self.order = order
+        self.product = product
