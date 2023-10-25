cd2765d044e3be6903d06e927379a942b6d79c04
bruno <brunosgazineu@hotmail.com>
2023-10-25 20:24:46 -0300

    feat: order implementation

--- models/order.py
+++ models/order.py
@@ -1,2 +1,15 @@
+from models.client import Client
+
+
 class Order:
-    ...
+    def __init__(
+        self,
+        *,
+        total_price: float,
+        status: str,
+        client: Client
+    ):
+        self.total_price = total_price
+        self.status = status
+        self.client = client
+        
