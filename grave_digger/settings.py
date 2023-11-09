49dfdf3294dd10090a2349b9efa5d046a9e33b62
Bruna Mendonça <bruna.von@outlook.com>
2023-11-08 21:49:25 -0300

    feat: create app socialnetworks

--- grave_digger/settings.py
+++ grave_digger/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'categories.apps.CategoriesConfig',
+    'categories.apps.CategoriesConfig', 
+    'socialnetworks.apps.SocialnetworksConfig',
 ]
 
 MIDDLEWARE = [
