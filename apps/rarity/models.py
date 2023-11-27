c308a1732389e833747653d1ff664ffebdc189f6
Luan Pereira Nordio <101523175+LNordio@users.noreply.github.com>
2023-11-27 20:39:45 -0300

    feat: rarity attributes

--- apps/rarity/models.py
+++ apps/rarity/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Rarity(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    base_multiplier = models.DecimalField(max_digits=5, decimal_places=2, default=1.00)
+
+    
