5db2f62b0b014bc55079f783276c64d69b8c561d
Luan Pereira Nordio <101523175+LNordio@users.noreply.github.com>
2023-11-27 20:48:09 -0300

    feat: implementation

--- apps/rarities/serializer.py
+++ apps/rarities/serializer.py
@@ -1,3 +1,7 @@
 from .models import Rarity 
 from rest_framework import serializers
 
+class RaritySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rarity
+        fields = '__all__'
