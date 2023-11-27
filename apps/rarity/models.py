a5d2b9c253933bd9e08b7ecd286615530a9806ef
Luan Pereira Nordio <101523175+LNordio@users.noreply.github.com>
2023-11-27 20:40:31 -0300

    feat: rarity metadata

--- apps/rarity/models.py
+++ apps/rarity/models.py
@@ -7,4 +7,9 @@ class Rarity(models.Model):
     description = models.TextField()
     base_multiplier = models.DecimalField(max_digits=5, decimal_places=2, default=1.00)
 
-    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Rarities'
+        ordering = ['name']
