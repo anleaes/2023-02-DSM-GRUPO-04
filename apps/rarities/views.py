9ff00ce41c076ce0fbec2f8c7976a5eebfeadee1
Luan Pereira Nordio <101523175+LNordio@users.noreply.github.com>
2023-11-27 20:51:54 -0300

    feat: implementation

--- apps/rarities/views.py
+++ apps/rarities/views.py
@@ -3,5 +3,7 @@ from rest_framework import viewsets
 from .serializer import RaritySerializer
 # Create your views here.
 
-
+class RarityViewSet(viewsets.ModelViewSet):
+    queryset = Rarity.objects.all()
+    serializer_class = RaritySerializer
 
