9e7deb34ef6bb928bfb281e14ccc31a9fe69b7f2
Bruna Mendonça <bruna.von@outlook.com>
2023-11-27 21:12:08 -0300

    feat: effects atributes

--- apps/effects/models.py
+++ apps/effects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Effect(models.Model):
+    TIPOS = [
+        ('H', 'healing'), ('D', 'damage')
+    ]
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    type =  models.CharField(choices= TIPOS, max_length=1)
