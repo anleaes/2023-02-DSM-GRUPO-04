bd8a2145e341d298f4392c2d078c07caf300be07
Luan Pereira Nordio <101523175+LNordio@users.noreply.github.com>
2023-11-29 20:47:32 -0300

    feat: items metadata

--- apps/items/models.py
+++ apps/items/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
+from apps.rarities.models import Rarity
 # Create your models here.
 
 class Item(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
-    category = models.ForeignKey(max_length=50)
-    rarity = models.ForeignKey(max_length=50)
+    category = models.ForeignKey()
+    rarity = models.ForeignKey(Rarity, on_delete=models.SET_NULL, null=True)
     value = models.FloatField()
     weight = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Items'
+        ordering = ['name']
