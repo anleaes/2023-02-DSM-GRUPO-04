6bc20597b33e56fa190c63636a7e8eef4203e7fb
Luan Pereira Nordio <101523175+LNordio@users.noreply.github.com>
2023-11-29 19:40:51 -0300

    feat: item attributes

--- apps/items/models.py
+++ apps/items/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    category = models.ForeignKey(max_length=50)
+    rarity = models.ForeignKey(max_length=50)
+    value = models.FloatField()
+    weight = models.FloatField()
