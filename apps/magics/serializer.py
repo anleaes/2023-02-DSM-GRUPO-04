4aa1418aec52112586b044fcc9c9ff32f448a877
Bruna Mendonça <bruna.von@outlook.com>
2023-11-29 21:17:44 -0300

    feat: serializer implementation

--- apps/magics/serializer.py
+++ apps/magics/serializer.py
@@ -1,2 +1,7 @@
 from .models import Magic
 from rest_framework import serializers
+
+class MagicSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = Magic
+        fields = '__all__'
