26a995dd3ee944f9db8e78604e69c99fadff2ecc
Bruna Mendonça <bruna.von@outlook.com>
2023-11-29 21:11:43 -0300

    feat: models attribute

--- apps/magics/models.py
+++ apps/magics/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Magic(models.Model):
+    
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    power = models.PositiveIntegerField()
+    mana_cost = models.PositiveIntegerField() 
+    duration = models.PositiveIntegerField() 
+
+   
