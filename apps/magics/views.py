6bad76b180e7d85e47ced95925160f3fb8e74c57
Bruna Mendonça <bruna.von@outlook.com>
2023-11-29 21:22:52 -0300

    feat: magic serializer implementation

--- apps/magics/views.py
+++ apps/magics/views.py
@@ -1,3 +1,9 @@
 from .models import Magic
 from rest_framework import viewsets
 from .serializer import MagicSerializer
+# Create your views here.
+
+
+class MagicViewSet (viewsets.ModelViewSet):
+    queryset = Magic.objects.all()
+    serializer_class = MagicSerializer
