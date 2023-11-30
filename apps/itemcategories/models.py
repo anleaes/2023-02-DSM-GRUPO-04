78a7e315e26b1522edbc0d0ee37cd7d537eed976
Luan Pereira Nordio <101523175+LNordio@users.noreply.github.com>
2023-11-29 21:09:34 -0300

    feat: models metadata

--- apps/itemcategories/models.py
+++ apps/itemcategories/models.py
@@ -7,3 +7,10 @@ class ItemCategory(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     max_stack = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name   
+    
+    class Meta:
+        verbose_name_plural = 'Item Categories'
+        ordering = ['name']
