ff14314daf781296771e29c4eceaa6ae142c8a65
bruno <brunosgazineu@hotmail.com>
2023-12-03 20:23:07 -0300

    feat: players metadata

--- apps/players/models.py
+++ apps/players/models.py
@@ -7,3 +7,10 @@ class Player(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
     character = models.OneToOneField(Character)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name_plural = 'Players'
+        ordering = ['first_name']
