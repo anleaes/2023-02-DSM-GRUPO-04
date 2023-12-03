6f1c7f4f06acf3a61ff9ac24f7a645f7dda25b1b
bruno <brunosgazineu@hotmail.com>
2023-12-03 20:01:58 -0300

    feat: inventory implementation

--- apps/inventories/models.py
+++ apps/inventories/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from characters.models import Character
 # Create your models here.
+
+class Inventory(models.Model):
+    size = models.IntegerField(default=10)
+    money = models.FloatField()
+    owner = models.OneToOneField(Character)
+
+    def __str__(self):
+        return f"{self.owner}'s inventory"
+    
+    class Meta:
+        verbose_name_plural = 'Inventories'
+        ordering = ['owner']
