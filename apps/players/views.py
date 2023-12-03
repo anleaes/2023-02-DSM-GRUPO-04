d166c07d7d404486e03fdb01ff53cbf745a82543
bruno <brunosgazineu@hotmail.com>
2023-12-03 20:26:11 -0300

    feat: views implementation

--- apps/players/views.py
+++ apps/players/views.py
@@ -3,3 +3,7 @@ from rest_framework import viewsets
 from .models import Player
 from .serializers import PlayerSerializer
 # Create your views here.
+
+class PlayerViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
