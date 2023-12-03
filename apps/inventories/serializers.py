3681955d5e87b8a4e826b465e52a244cbf3a180f
bruno <brunosgazineu@hotmail.com>
2023-12-03 20:08:17 -0300

    feat: inventory slot serializer

--- apps/inventories/serializers.py
+++ apps/inventories/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Inventory
+from .models import Inventory, InventorySlot
 
 class InventorySerializer(serializers.ModelSerializer):
     class Meta:
         queryset = Inventory.objects.all()
-        fields = "__all__" 
+        fields = "__all__"
+
+class InventorySlotSerializer(serializers.ModelSerializer):
+    class Meta:
+        queryset = InventorySlot.objects.all()
+        fields = "__all__"
