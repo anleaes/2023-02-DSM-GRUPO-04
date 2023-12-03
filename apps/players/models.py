b9da190d29ababb01a170ba1f70f8361dd7c50c7
bruno <brunosgazineu@hotmail.com>
2023-12-03 20:21:56 -0300

    feat: character attributes

--- apps/players/models.py
+++ apps/players/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from characters.models import Character
 # Create your models here.
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    character = models.OneToOneField(Character)
