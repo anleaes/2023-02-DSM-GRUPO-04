7a53e4c8c92f5d586743a7df58e3dac22c30324f
bruno <brunosgazineu@hotmail.com>
2023-12-03 20:31:41 -0300

    fix: serializers

--- apps/rarities/views.py
+++ apps/rarities/views.py
@@ -1,6 +1,6 @@
 from .models import Rarity
 from rest_framework import viewsets
-from .serializer import RaritySerializer
+from .serializers import RaritySerializer
 # Create your views here.
 
 class RarityViewSet(viewsets.ModelViewSet):
