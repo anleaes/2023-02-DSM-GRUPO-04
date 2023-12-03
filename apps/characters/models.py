f3e9c6c66108e32e2e368f7901a457656a72883d
bruno <brunosgazineu@hotmail.com>
2023-12-03 19:42:53 -0300

    feat: model attributes

--- apps/characters/models.py
+++ apps/characters/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+from magics.models import Magic
+
 # Create your models here.
+
+class Character(models.Model):
+    name = models.CharField(max_length=100)
+    level = models.IntegerField()
+    magics = models.ManyToManyField(Magic)
+    inventory = models.ForeignKey()
